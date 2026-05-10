663e79ae30fb94a54f0c3252e776a4836256ddc7
0AlexLoborchuk <loborchukolex@gmail.com>
2026-05-10 11:29:22 +0300

    add adaptive to tablet

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log("Hello world");
+let viewWidth = window.innerWidth;
+let headerButton = document.querySelector("#headerButton");
+if (viewWidth > 799) {
+  headerButton.innerHTML = "Book an appointment";
+}
