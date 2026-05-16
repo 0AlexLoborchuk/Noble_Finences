24c011a43f1bc333d07c5ff75c7eee5b396a22e4
0AlexLoborchuk <loborchukolex@gmail.com>
2026-05-16 17:57:31 +0300

    add fonts and add js to comment block

--- js/script.js
+++ js/script.js
@@ -3,3 +3,35 @@ let headerButton = document.querySelector("#headerButton");
 if (viewWidth > 799) {
   headerButton.innerHTML = "Book an appointment";
 }
+
+feedback_item = document.querySelectorAll(".feedback__items");
+comment = document.querySelector(".comment");
+autor_icon = document.querySelector(".author__icon");
+autor_name = document.querySelector(".author__name");
+function changeDescription(item) {
+  if (item == "Graphic Designer") {
+    feedback_item[1].classList.remove("feedback__items_active");
+    feedback_item[2].classList.remove("feedback__items_active");
+    feedback_item[0].classList.add("feedback__items_active");
+    comment.innerHTML =
+      '"Managing my taxes as a freelancer used to be overwhelming, but Noble Finance made it effortless.”';
+    autor_icon.setAttribute("src", "./image/icons/Taylor.png");
+    autor_name.innerHTML = "Taylor M., 28 years old";
+  } else if (item == "Commercial Photographer") {
+    feedback_item[0].classList.remove("feedback__items_active");
+    feedback_item[2].classList.remove("feedback__items_active");
+    feedback_item[1].classList.add("feedback__items_active");
+    comment.innerHTML =
+      '"From invoicing to tax prep, Noble Finance has been a game-changer for my freelance business.”';
+    autor_icon.setAttribute("src", "./image/icons/Markus.png");
+    autor_name.innerHTML = "Markus N., 31 years old";
+  } else {
+    feedback_item[0].classList.remove("feedback__items_active");
+    feedback_item[1].classList.remove("feedback__items_active");
+    feedback_item[2].classList.add("feedback__items_active");
+    comment.innerHTML =
+      '"I used to worry about missing deductions, but with Noble Finance, I know I’m maximizing my savings.”';
+    autor_icon.setAttribute("src", "./image/icons/Margaret_L.png");
+    autor_name.innerHTML = "Margaret L., 24 years old";
+  }
+}
